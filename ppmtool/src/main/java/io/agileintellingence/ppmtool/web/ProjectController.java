2ccee1826bc4d6b331eaaf8b444a545e6aa0efad
Yasin Ekinci <ekinciyasin34@gmail.com>
2021-12-31 07:58:42 +0100

    BindingResult

--- ppmtool/src/main/java/io/agileintellingence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintellingence/ppmtool/web/ProjectController.java
@@ -1,14 +1,18 @@
 package io.agileintellingence.ppmtool.web;
 
 import io.agileintellingence.ppmtool.domain.Project;
+import io.agileintellingence.ppmtool.services.MapValidationErrorService;
 import io.agileintellingence.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +21,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+
         projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
